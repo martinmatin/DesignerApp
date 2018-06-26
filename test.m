95bf4c7c6705aa68d773a3c10c9f919efb48537d
unknown <martin.crappe@gmail.com>
2018-06-26 03:06:10 +0200

    added live gauge

--- test.m
+++ test.m
@@ -1,21 +1,5 @@
-% C = {1223,120,'RPM',5;
-%     1224,100,'VITESSE',130};
-% 
-% C
-% 
-% % index = find([C{:,2}] == 100)
-% % isempty(index)
-% %  [index,~] =size(C)
-% % 
-% % %C{index,4} = 140;
-% C
-% 
-% disp()
 
-tic
-pause(5);
-disp(toc)
+idInfo(50,:) = {'','',''};
 
-tic
-pause(3);
-disp(toc);
+idInfo(50,:) = {'RPM','Tour/min',10000};
+iddd =repmat({'','',''},10,1)
